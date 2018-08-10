31a4ffc89a48fd4169c3d378d8a6a7f2d7b39c4a
ADolodarenko <adolodar@gmail.com>
2018-08-11 01:33:21 +0300

    Test bubbles.

--- src/BubblePanel.java
+++ src/BubblePanel.java
@@ -1,10 +1,45 @@
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.util.Random;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class BubblePanel extends JPanel
 {
 	Random rand = new Random();
+	ArrayList<Bubble> bubbleList;
+	int size = 25;
+	
+	public BubblePanel()
+	{
+		bubbleList = new ArrayList<>();
+		setBackground(Color.BLACK);
+		
+		testBubbles();
+	}
+	
+	@Override
+	public void paintComponent(Graphics canvas)
+	{
+		super.paintComponent(canvas);
+		
+		for (Bubble b : bubbleList)
+			b.draw(canvas);
+	}
+	
+	public void testBubbles()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			int x = rand.nextInt(600);
+			int y = rand.nextInt(400);
+			int size = rand.nextInt(50);
+			
+			bubbleList.add(new Bubble(x, y, size));
+		}
+		
+		repaint();
+	}
 	
 	private class Bubble
 	{
@@ -21,6 +56,12 @@ public class BubblePanel extends JPanel
 			color = new Color(rand.nextInt(256),
 					rand.nextInt(256),
 					rand.nextInt(256));
+		}
+		
+		public void draw(Graphics canvas)
+		{
+			canvas.setColor(color);
+			canvas.fillOval(x - size/2, y - size/2, size, size);
 		}
 	}
 
